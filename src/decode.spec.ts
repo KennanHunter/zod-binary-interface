30131523ad9c377e1c1a3ab003b78b78688f0dbb
Kennan Hunter <kennanhunter5@gmail.com>
2023-05-05 15:18:12 -0400

    not sure why tests are not passing ngl

--- src/decode.spec.ts
+++ src/decode.spec.ts
@@ -14,4 +14,22 @@ describe("data decoding", () => {
     // expect(() => schema.parse(decodedData)).toReturn();
     expect(decodedData).toEqual([{ path: [], value: 69 }]);
   });
+  test("simple number and short string", () => {
+    const schema = z.object({
+      num: z.number(),
+      str: z.string(),
+    });
+
+    const mockData = new Uint8Array([
+      // 69, "H", "i", null terminator
+      0b01000101, 0b0100_1000, 0b01101001, 0b0000_0000,
+    ]);
+
+    const decodedData = decode(mockData, flattenSchema(schema));
+    // expect(() => schema.parse(decodedData)).toReturn();
+    expect(decodedData).toEqual([
+      { path: [], value: 69 },
+      { path: [], value: "Hi" },
+    ]);
+  });
 });
--- src/decode.ts
+++ src/decode.ts
@@ -1,28 +1,63 @@
+type BlockDecodeResult = { path: string[]; value: any };
 export const decode = (data: Uint8Array, blocks: Block[]): unknown => {
+  const BITS_PER_ELEMENT = data.BYTES_PER_ELEMENT * 8;
+
   let curBit: number = 0;
 
-  const curByte = () => Math.floor(curBit / data.BYTES_PER_ELEMENT);
+  const curByte = () => Math.floor(curBit / BITS_PER_ELEMENT);
   const getData = (startingBit: number, length: number) => {
     if (
-      length % data.BYTES_PER_ELEMENT === 0 &&
-      startingBit % data.BYTES_PER_ELEMENT === 0
+      length % BITS_PER_ELEMENT === 0 &&
+      startingBit % BITS_PER_ELEMENT === 0
     ) {
+      console.log("gettind data", startingBit, length);
       return data.subarray(
-        startingBit / data.BYTES_PER_ELEMENT,
-        length / data.BYTES_PER_ELEMENT
+        startingBit / BITS_PER_ELEMENT,
+        startingBit / BITS_PER_ELEMENT + length / BITS_PER_ELEMENT
       );
     }
   };
 
-  return blocks.map((block): undefined | { path: string[]; value: any } => {
+  return blocks.map((block): undefined | BlockDecodeResult => {
     if (block.block === "discriminator") return;
 
     if (block.type === "number") {
       const numData = getData(curBit, 8)?.at(0);
+      curBit += 8;
 
       if (!numData) return;
 
       return { path: [], value: numData };
+    }
+
+    if (block.type === "string") {
+      const restOfData = getData(
+        curBit,
+        // get subarray of rest of stream
+        data.length * BITS_PER_ELEMENT - curBit
+      );
+
+      if (!restOfData) return;
+
+      let index = 0;
+      for (const value of restOfData) {
+        index += 1;
+        if (value === 0b00000000) {
+          index += 1;
+          break;
+        }
+      }
+
+      const stringData = getData(curBit, index * BITS_PER_ELEMENT);
+
+      console.log(stringData);
+
+      const str = new TextDecoder().decode(stringData);
+
+      return {
+        path: [],
+        value: str,
+      };
     }
   });
 };
